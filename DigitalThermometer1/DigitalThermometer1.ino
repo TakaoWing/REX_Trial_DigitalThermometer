256186358211d3215bf725718f5915a0a08d786a
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2022-01-20 14:54:47 +0900

    Initial commit

---
+++ DigitalThermometer1/DigitalThermometer1.ino
@@ -0,0 +1,13 @@
+/*デジタル温度計を作ろう！
+ * 内容
+ * 温度と湿度をシリアルモニタに表示する
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
